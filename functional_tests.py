cb25f39880f86e9d0e0f80aed05132f0a979834d
Yngve Høiseth <yhoiseth@gmail.com>
2017-02-22 20:01:58 +0100

    Add dummy data on POST

--- functional_tests.py
+++ functional_tests.py
@@ -31,10 +31,23 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table.'
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
+
+
+
 
         self.fail('Finish the test.')
 
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    # if request.method == 'POST':
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', '')
+    })
+    # return render(request, 'home.html')
